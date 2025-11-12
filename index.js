1f3059ebe54897af2faf57f923cbc1ecbd60a9bc
Safa Besrour <besrour.safa.7@gmail.com>
2025-11-12 17:50:44 +0100

    update horsePower

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ var Car = /** @class */ (function () {
     Car.prototype.start = function () {
         console.log("Car engine started");
     };
+    Car.prototype.addHorsePower = function (puissance) {
+        this.horsePower = puissance;
+    };
     return Car;
 }());
 var carOne = new Car('Renault', 'clio4', 2016);
+console.log(carOne);
+carOne.addHorsePower(150);
 console.log(carOne);
 carOne.start();
--- index.tsx
+++ index.tsx
@@ -2,26 +2,36 @@ interface Vehicle {
 make : string; 
 model : string ; 
 year : number ; 
+horsePower? : number; 
 start(): void ;
+addHorsePower (horsePower: number) : void; 
 }
 class Car implements Vehicle {
 make : string; 
 model : string ; 
 year : number ; 
+horsePower? : number
+
+
 
 constructor(make: string , model : string, year: number ) {
     this.make = make
     this.model = model
     this.year = year
+
   }
 
  start() {
     console.log("Car engine started");
-
-
  }
+addHorsePower (puissance: number) {
+    this.horsePower = puissance
+}
 
 }
+
 const carOne = new Car ('Renault', 'clio4', 2016)
 console.log(carOne);
+carOne.addHorsePower(150) 
+console.log(carOne)
 carOne.start () 
